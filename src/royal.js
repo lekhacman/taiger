a6f5f95daa2ff0d6fab8a2b9126904ffe7df6070
andrew <khacmanle@gmail.com>
2019-10-05 09:51:58 +0800

    [Question 2] complete implementation

--- src/royal.js
+++ src/royal.js
@@ -1,3 +1,69 @@
-export function getSortedList(names) {
-  return names;
+import * as R from 'ramda';
+
+export const getSortedList = R.pipe(
+  R.map(R.split(' ')),
+  R.sortWith([
+    R.ascend(R.prop(0)),
+    R.ascend(
+      R.compose(
+        toInteger,
+        R.prop(1)
+      )
+    ),
+  ]),
+  R.map(R.join(' '))
+);
+
+const ROMAN = {
+  SINGLE: {
+    I: 1,
+    II: 2,
+    III: 3,
+    IV: 4,
+    V: 5,
+    VI: 6,
+    VII: 7,
+    VIII: 8,
+    IX: 9,
+    X: 10,
+  },
+  DOUBLE: {
+    X: 10,
+    XX: 20,
+    XXX: 30,
+    XL: 40,
+    L: 50,
+  },
+};
+const ROMAN_FLAT = R.pipe(
+  Object.values,
+  R.apply(R.mergeRight)
+)(ROMAN);
+
+export function toInteger(romanChars) {
+  const singleLens = R.lensIndex(0);
+  const doubleLens = R.lensIndex(1);
+
+  return R.pipe(
+    R.split(''),
+    R.reduceRight(
+      (char, acc) =>
+        R.ifElse(
+          R.compose(
+            R.flip(R.has)(ROMAN.SINGLE),
+            R.concat(char)
+          ),
+          R.always(R.over(singleLens, R.concat(char), acc)),
+          R.always(R.over(doubleLens, R.concat(char), acc))
+        )(R.view(singleLens, acc)),
+      ['', '']
+    ),
+    R.map(
+      R.compose(
+        R.defaultTo(0),
+        R.flip(R.prop)(ROMAN_FLAT)
+      )
+    ),
+    R.reduce(R.add, 0)
+  )(romanChars);
 }
--- src/royal.test.js
+++ src/royal.test.js
@@ -1,16 +1,75 @@
-import { getSortedList } from './royal';
+import { getSortedList, toInteger } from './royal';
 
 describe('Royal Rumble', function() {
   const cases = [
     {
-      input: [],
-      output: [],
+      input: ['Louis IX', 'Louis VIII', 'David II'],
+      output: ['David II', 'Louis VIII', 'Louis IX'],
     },
   ];
 
   cases.forEach((c, id) => {
     test(`#${id} getSortedList`, function() {
       expect(getSortedList(c.input)).toEqual(c.output);
+    });
+  });
+});
+
+describe('Roman parser', function() {
+  const cases = [
+    {
+      input: 'I',
+      output: 1,
+    },
+    {
+      input: 'II',
+      output: 2,
+    },
+    {
+      input: 'V',
+      output: 5,
+    },
+    {
+      input: 'VII',
+      output: 7,
+    },
+    {
+      input: 'IX',
+      output: 9,
+    },
+    {
+      input: 'X',
+      output: 10,
+    },
+    {
+      input: 'XIII',
+      output: 13,
+    },
+    {
+      input: 'XXII',
+      output: 22,
+    },
+    {
+      input: 'XXXIV',
+      output: 34,
+    },
+    {
+      input: 'XLVII',
+      output: 47,
+    },
+    {
+      input: 'L',
+      output: 50,
+    },
+    {
+      input: 'LV',
+      output: 55,
+    },
+  ];
+
+  cases.forEach((c, id) => {
+    test(`#${id} getSortedList`, function() {
+      expect(toInteger(c.input)).toEqual(c.output);
     });
   });
 });
